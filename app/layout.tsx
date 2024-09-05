11e790bfcd14add84f76e7e20bde04d9ceb6d43f
Ameen A <abulhashim351@gmail.com>
2024-09-05 14:04:45 +0500

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Jaiza',
+  description: '',
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
