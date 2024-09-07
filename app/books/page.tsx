9d8e2b2cad26acf9761b5d1c490278c0540f7b75
Ameen A <abulhashim351@gmail.com>
2024-09-07 15:28:28 +0500

    Refactor BooksPage component to include Empty component for displaying an empty list

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,3 +1,12 @@
+import Empty from '@/components/books/Empty'
+
 export default function BooksPage() {
-  return <div>BooksPage</div>
+  return (
+    <div className="flex min-h-[calc(100vh_-_theme(spacing.16))] w-full flex-1 flex-col gap-4 bg-muted/40 p-4 md:min-h-screen md:gap-8 md:p-10">
+      <div className="mx-auto grid w-full max-w-6xl gap-2">
+        <h1 className="text-3xl font-semibold">Books</h1>
+      </div>
+      <Empty />
+    </div>
+  )
 }
---
+++ components/books/Empty.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import { Plus, LibraryBig } from 'lucide-react'
+
+export default function Empty() {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <LibraryBig className="mb-4 h-12 w-12" />
+      <h1 className="mb-1 text-xl font-medium">Your list is empty!</h1>
+      <p className="mb-2 text-muted-foreground">Click + button to add books </p>
+      <Button className="px-12">
+        <Plus className="me-2 h-5 w-5" />
+        Add New Book
+      </Button>
+    </div>
+  )
+}
